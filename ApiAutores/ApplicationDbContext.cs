1ccf0b7b483a0706048129215d939a5aa4568eba
aitageo <aitageo@DESKTOP-QKQQAHU>
2022-07-25 16:50:52 -0500

    Agregar archivos de proyecto.

---
+++ ApiAutores/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiAutores.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiAutores
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet <Autor>  Autores { get; set; }
+    }
+
+}
---
+++ ApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,19 @@
+﻿using ApiAutores.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() {
+                new Autor() {Id=1 ,Nombre ="Alfonso" },
+                new Autor() {Id=2 ,Nombre ="Gloria" }
+            };
+        }
+    }
+}
